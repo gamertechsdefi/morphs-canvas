bdd97c5970463eee7f0c9514dbf6c40241a99e86
Gamer <gamertechs.defi@gmail.com>
2025-08-09 20:29:42 +0100

    added url to front end

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
         "*",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://morphs-canvas.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
